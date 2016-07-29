6a817f347fef268d4660d6830285eafbd95cc57c
Matthew Graham <mdg149@gmail.com>
2016-07-29 22:47:27 +0200

    new python unit tests to run the scripts in T/
    can use these to hook up to automatic CI so I can find out when I break things

---
+++ test.py
@@ -0,0 +1,58 @@
+import unittest
+import subprocess
+
+def run_leema(f):
+    args = ["target/debug/leema", "T/"+f+".lma"]
+    print(args)
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+    result = proc.wait()
+    output = proc.stdout.read()
+    return {'code': result, 'output': output}
+
+class TestScripts(unittest.TestCase):
+    def test_cout(self):
+        result = run_leema('cout_quotes')
+        self.assertEqual(0, result['code'])
+        self.assertEqual(b"hello \"quotes\"\n", result['output'])
+
+    def test_fact_match(self):
+        result = run_leema('fact_match')
+        self.assertEqual(0, result['code'])
+        self.assertEqual(b"factorial(4) = 24\n", result['output'])
+
+    def test_factorial(self):
+        result = run_leema('factorial')
+        self.assertEqual(0, result['code'])
+        self.assertEqual(b"factorial(4) = 24\n", result['output'])
+
+    def test_fizzbuzz(self):
+        result = run_leema('fizzbuzz')
+        self.assertEqual(0, result['code'])
+        lines = result['output'].strip().splitlines()
+        self.assertEqual(b"1", lines[0])
+        self.assertEqual(b"2", lines[1])
+        self.assertEqual(b"fizz", lines[2])
+        self.assertEqual(b"4", lines[3])
+        self.assertEqual(b"buzz", lines[4])
+        self.assertEqual(b"fizz", lines[5])
+        self.assertEqual(b"7", lines[6])
+
+        self.assertEqual(b"14", lines[13])
+        self.assertEqual(b"fizzbuzz", lines[14])
+        self.assertEqual(b"16", lines[15])
+
+        self.assertEqual(b"buzz", lines[-6])
+        self.assertEqual(b"fizz", lines[-5])
+        self.assertEqual(b"97", lines[-4])
+        self.assertEqual(b"98", lines[-3])
+        self.assertEqual(b"fizz", lines[-2])
+        self.assertEqual(b"buzz", lines[-1])
+
+    def test_func_3params(self):
+        result = run_leema('func_3params')
+        self.assertEqual(0, result['code'])
+        self.assertEqual(b"sum(3, 8, 2) = 13\n", result['output'])
+
+
+if __name__ == '__main__':
+    unittest.main()
