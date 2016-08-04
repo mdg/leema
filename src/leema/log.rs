78be23a77b53dd9cc6e4818be09fac7c7ddb1f45
Matthew Graham <mdg149@gmail.com>
2016-08-05 00:12:30 +0200

    duplicated verbose_out as vout
    vout is more in line w/ cout. will switch usages over as I go.

--- src/leema/log.rs
+++ src/leema/log.rs
@@ -16,6 +16,21 @@ pub fn is_verbose() -> bool
 }
 
 #[macro_export]
+macro_rules! vout
+{
+    ($fmt:expr) => {
+        if log::is_verbose() {
+            write!(stderr(), $fmt).ok();
+        }
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        if log::is_verbose() {
+            (write!(stderr(), $fmt, $($arg)*)).ok();
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! verbose_out
 {
     ($fmt:expr) => {
