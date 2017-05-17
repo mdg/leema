52619188284408bdbd281fc39ff7f855d9f5c887
Matthew Graham <mdg149@gmail.com>
2017-05-17 02:01:59 +0000

    remove worker usages of fiber
    might add back fiber next

--- src/leema/worker.rs
+++ src/leema/worker.rs
@@ -1,7 +1,6 @@
 
 use leema::code::{self, CodeKey, Code, Op, OpVec, ModSym, RustFunc};
 use leema::frame::{self, Event, Frame, Parent};
-use leema::fiber::{Fiber, FiberToWorkerMsg, WorkerToFiberMsg};
 use leema::log;
 use leema::reg::{Reg};
 use leema::val::{Env, Val, MsgVal};
@@ -19,8 +18,6 @@ use std::time::{Duration};
 use futures::{Poll, Async, Stream, Sink};
 use futures::future::{Future};
 use futures::task;
-use futures::unsync;
-use futures::unsync::mpsc;
 use tokio_core::reactor;
 
 
@@ -86,9 +83,6 @@ pub struct Worker
     event: Event,
     app_tx: Sender<Msg>,
     app_rx: Receiver<Msg>,
-    send_from_fiber: unsync::mpsc::Sender<FiberToWorkerMsg>,
-    recv_from_fiber: unsync::mpsc::Receiver<FiberToWorkerMsg>,
-    send_to_fiber: HashMap<i64, unsync::mpsc::Sender<WorkerToFiberMsg>>,
     //io: IOQueue,
     id: i64,
     next_fiber_id: i64,
@@ -108,7 +102,6 @@ impl Worker
     {
         let mut core = reactor::Core::new().unwrap();
         let h = core.handle();
-        let (tx, rx) = unsync::mpsc::channel(1000);
         let w = Worker{
             fresh: LinkedList::new(),
             waiting: HashMap::new(),
@@ -117,9 +110,6 @@ impl Worker
             event: Event::Uneventful,
             app_tx: send,
             app_rx: recv,
-            send_from_fiber: tx,
-            recv_from_fiber: rx,
-            send_to_fiber: HashMap::new(),
             id: wid,
             next_fiber_id: 0,
             done: false,
@@ -168,10 +158,6 @@ impl Worker
                 let mut new_mod = HashMap::new();
                 new_mod.insert(func, rc_code.clone());
                 self.code.insert(module, new_mod);
-                let response = WorkerToFiberMsg::FoundCode(rc_code);
-                let mut sender = self.send_to_fiber.get_mut(&fiber_id).unwrap();
-                sender.start_send(response);
-                sender.poll_complete();
             }
             _ => {
                 panic!("Must be a message for the app: {:?}", msg);
@@ -184,11 +170,6 @@ impl Worker
         let id = self.next_fiber_id;
         self.next_fiber_id += 1;
         let frame = Frame::new_root(module, func);
-        let (send_to_fiber, recv_from_worker) =
-            unsync::mpsc::channel::<WorkerToFiberMsg>(10);
-        self.send_to_fiber.insert(id, send_to_fiber);
-        Fiber::spawn(&self.handle, id, frame
-            , self.send_from_fiber.clone(), recv_from_worker);
     }
 
     fn pop_fresh(&mut self) -> Option<(Rc<Code>, Frame)>
@@ -205,33 +186,6 @@ impl Worker
     {
 vout!("lock app, add_fork\n");
     }
-
-    fn process_msg_from_fiber(&mut self, msg: FiberToWorkerMsg)
-    {
-        match msg {
-            FiberToWorkerMsg::RequestCode(fid, module, func) => {
-                let mut found = false;
-                if let Some(msrc) = self.code.get(&module) {
-                    if let Some(mfunc) = msrc.get(&func) {
-                        let response =
-                            WorkerToFiberMsg::FoundCode(mfunc.clone());
-                        let mut sender =
-                            self.send_to_fiber.get_mut(&fid).unwrap();
-                        sender.start_send(response);
-                        sender.poll_complete();
-                        found = true;
-                    }
-                }
-                if !found {
-                    let msg = Msg::RequestCode(self.id, fid, module, func);
-                    self.app_tx.send(msg);
-                }
-            }
-            FiberToWorkerMsg::MainResult(res) => {
-                println!("whoa main finished! {:?}", res);
-            }
-        }
-    }
 }
 
 // struct Iterate { w: Worker }
@@ -244,26 +198,6 @@ impl Future for Worker
 
     fn poll(&mut self) -> Poll<Val, Val>
     {
-        loop {
-            match self.recv_from_fiber.poll() {
-                Result::Ok(Async::NotReady) => {
-                    // println!("recv_from_fiber(Async::NotReady)");
-                    break;
-                }
-                Result::Ok(Async::Ready(None)) => {
-                    println!("recv_from_fiber(Async::Ready(None))");
-                    break;
-                }
-                Result::Ok(Async::Ready(Some(msg))) => {
-                    println!("received msg: {:?}", msg);
-                    self.process_msg_from_fiber(msg);
-                }
-                Result::Err(err) => {
-                    println!("recv_from_fiber err: {:?}", err);
-                }
-            }
-        }
-
         self.run_once();
         thread::yield_now();
 
