e886a88c5892bd5bae57ae88e6f33704d3a92cdc
Matthew Graham <mdg149@gmail.com>
2017-10-19 02:11:04 +0000

    implement file::write_file()

--- src/leema/file.rs
+++ src/leema/file.rs
@@ -4,7 +4,7 @@ use leema::log;
 use leema::rsrc;
 use leema::val::{Val};
 
-use std::fs::{File};
+use std::fs::{File, OpenOptions};
 use std::io::{self, stderr, Read, Write};
 use std::path::{Path};
 
@@ -35,6 +35,15 @@ pub fn file_write(mut ctx: rsrc::IopCtx) -> rsrc::Event
 pub fn file_write_file(mut ctx: rsrc::IopCtx) -> rsrc::Event
 {
     vout!("file_write_file()\n");
+    let pathval = ctx.take_param(0).unwrap();
+    let output = ctx.take_param(1).unwrap();
+    let path = Path::new(pathval.str());
+    let mut f = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path).unwrap();
+    f.write_all(output.str().as_bytes());
     rsrc::Event::Success(Val::Void, None)
 }
 
@@ -42,7 +51,7 @@ pub fn load_rust_func(func_name: &str) -> Option<Code>
 {
     match func_name {
         "read_file" => Some(Code::Iop(file_read_file, None)),
-        "write_file" => Some(Code::Iop(file_read_file, None)),
+        "write_file" => Some(Code::Iop(file_write_file, None)),
         _ => None,
     }
 }
