18852cc7bc05aaf1b9b929d25aaf2fa3668c4f11
Matthew Graham <mattg@leema.org>
2018-10-09 08:29:15 -0700

    moved start_fork/join_fork into the IO loop
    take advantage of existing blocking code will make it easier to join
    and recover the result of forks
    
    now getting a type error from somewhere. Type:Vars might be a busted
    concept and need to just look up type vars like looking up any other id

--- src/leema/lib_task.rs
+++ src/leema/lib_task.rs
@@ -1,8 +1,27 @@
 use leema::code::Code;
 use leema::frame::Event;
-use leema::struple::Struple;
-use leema::val::Val;
+use leema::io::RunQueueReceiver;
+use leema::lstr::Lstr;
+use leema::rsrc;
+use leema::val::{Type, Val};
 use leema::worker::RustFuncContext;
+
+use futures::future::Future;
+
+
+#[derive(Debug)]
+struct Lfuture
+{
+    receiver: RunQueueReceiver,
+}
+
+impl rsrc::Rsrc for Lfuture
+{
+    fn get_type(&self) -> Type
+    {
+        Type::future(Type::Var(Lstr::Sref("T")))
+    }
+}
 
 
 pub fn start(mut ctx: RustFuncContext) -> Event
@@ -21,30 +40,41 @@ pub fn start(mut ctx: RustFuncContext) -> Event
     Event::success()
 }
 
-pub fn start_fork(mut ctx: RustFuncContext) -> Event
+pub fn start_fork(mut ctx: rsrc::IopCtx) -> rsrc::Event
 {
     vout!("lib_task::start_fork()\n");
-    let receiver = match ctx.get_param(0) {
-        &Val::FuncRef(ref fri, ref args, _) => {
-            ctx.new_fork(fri.clone(), args.clone())
+    let receiver: RunQueueReceiver = match ctx.take_param(0).unwrap() {
+        Val::FuncRef(fri, args, _) => {
+            let runq = ctx.clone_run_queue();
+            runq.spawn(fri, args)
         }
         not_func => {
             panic!("start fork parameter is not a func: {:?}", not_func);
         }
     };
-    let result = Val::Tuple(Struple(vec![
-        (None, Val::future(receiver)),
-        (None, Val::Int(7)),
-    ]));
-    ctx.set_result(result);
-    Event::success()
+    let future_rsrc = Lfuture { receiver };
+    rsrc::Event::NewRsrc(Box::new(future_rsrc), None)
+}
+
+pub fn join_fork(mut ctx: rsrc::IopCtx) -> rsrc::Event
+{
+    let receiver: Lfuture = ctx.take_rsrc();
+    let rfut = receiver.receiver
+        .map(|result| {
+            rsrc::Event::Result(result, None)
+        })
+        .map_err(|e| {
+            rsrc::Event::Result(e, None)
+        });
+    rsrc::Event::Future(Box::new(rfut))
 }
 
 pub fn load_rust_func(func_name: &str) -> Option<Code>
 {
     match func_name {
         "start" => Some(Code::Rust2(start)),
-        "start_fork" => Some(Code::Rust2(start_fork)),
+        "start_fork" => Some(Code::Iop(start_fork, None)),
+        "join_fork" => Some(Code::Iop(join_fork, Some(0))),
         _ => None,
     }
 }
