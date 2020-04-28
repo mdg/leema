55088d682aa89a3b0391e2b43c35e5f7bc250172
Matthew Graham <mattg@leema.org>
2020-04-28 00:41:58 -0400

    fix test_proto_imports
    code was already good hopefully
    just made the tests match existing behavior

--- src/leema/proto.rs
+++ src/leema/proto.rs
@@ -1247,29 +1247,27 @@ mod tests
     {
         let proto = new_proto(
             "
-        import tacos
-        import >>
-            ./burritos
-            ./tortas >>
-                huevos >>
-                    .
-                    rancheros
-                --
+        import /tacos
+        import burritos
+        import tortas >>
+            huevos
+            huevos >>
+                rancheros
                 enchiladas
             --
-            ../nachos
         --
+        import ../nachos
         ",
         );
 
         assert_eq!(6, proto.imports.len());
         assert_eq!(0, proto.exports.len());
 
-        assert_eq!("tacos", proto.imports["tacos"]);
-        assert_eq!("./burritos", proto.imports["burritos"]);
-        assert_eq!("./tortas/huevos", proto.imports["huevos"]);
-        assert_eq!("./tortas/huevos/rancheros", proto.imports["rancheros"]);
-        assert_eq!("./tortas/enchiladas", proto.imports["enchiladas"]);
+        assert_eq!("/tacos", proto.imports["tacos"]);
+        assert_eq!("burritos", proto.imports["burritos"]);
+        assert_eq!("tortas/huevos", proto.imports["huevos"]);
+        assert_eq!("tortas/huevos/rancheros", proto.imports["rancheros"]);
+        assert_eq!("tortas/huevos/enchiladas", proto.imports["enchiladas"]);
         assert_eq!("../nachos", proto.imports["nachos"]);
     }
 
