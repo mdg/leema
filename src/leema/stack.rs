db62e16405b8d35fcca4ebece1d32bcb203f95b4
Matthew Graham <mattg@leema.org>
2021-12-12 01:57:22 -0500

    need to figure out how to know which ref to call

--- src/leema/stack.rs
+++ src/leema/stack.rs
@@ -1,19 +1,20 @@
-use crate::leema::failure::Lresult;
-use crate::leema::reg::Reg;
-use crate::leema::val::Val;
+use crate::leema::failure::{self, Lresult};
+use crate::leema::reg::{Ireg, Reg};
+use crate::leema::struple::Struple2;
+use crate::leema::val::{Fref, Val};
 
 /// StackBuffer stores the data for a particular fiber/task
 ///
 /// ### Call stack handling
 ///
-/// Push result reg       <-- frame base
-/// Push Fref
-/// Push Closed or Self
+/// Push Result Reg       <-- frame base
 /// Push Arg0
 /// ...
 /// Push ArgN
-/// Call
-/// Expand LocalsN w/ Void
+/// Push Closed or Self
+/// Push Fref
+/// Call     <- what's the call register?
+/// Expand Locals w/ Void
 /// Measure diff to frame base
 ///
 /// ### Call stack return
@@ -41,64 +42,125 @@ pub struct Buffer
 
 impl Buffer
 {
-    pub fn new(size: usize) -> Buffer
+    pub fn new(size: usize, f: Fref, args: Struple2<Val>) -> (Buffer, Ref)
     {
-        Buffer {
+        let mut b = Buffer {
             data: Vec::with_capacity(size),
-        }
+        };
+        let frame = b.push_frame_args(f, args);
+        (b, frame)
     }
 
-    pub fn frame(&mut self) -> Frame
+    fn push_frame_args(&mut self, f: Fref, args: Struple2<Val>) -> Ref
     {
+        // push result
         self.data.push(Val::VOID);
-        let func = self.data.last().unwrap();
+        let result_index = self.data.len();
 
-        Frame {
-            result: Val::VOID,
-            func,
-            args: &[],
-            vars: &mut [],
-            stack: self,
+        // push func
+        self.data.push(Val::Func(f));
+        let arg_0 = self.data.len();
+        // push args
+        for a in args.into_iter() {
+            self.data.push(a.v);
+        }
+
+        let stack: *mut Buffer = unsafe { self };
+        Ref {
+            param: &self.data[arg_0..self.data.len()],
+            local: &mut [],
+            result_index: 1,
+            stack,
         }
     }
 }
 
-#[derive(Debug)]
-pub struct Frame
+pub struct Ref
 {
-    result: Val,
-    // result_ptr: Option<*mut Val>,
-    // subj: Option<*mut Val>,
-    func: *const Val,
-    args: *const [Val],
-    vars: *mut [Val],
+    param: *const [Val],
+    local: *mut [Val],
+    result_index: usize,
     stack: *mut Buffer,
 }
 
-impl Frame
+impl Ref
 {
-    pub fn push_call(&mut self, _result: Reg, _call: Reg) // -> Lresult<Frame>
+    pub fn push_frame_args(&mut self, func: Fref, args: Struple2<Val>) -> Ref
     {
+        let stack: &mut Buffer = &mut unsafe { *self.stack };
+        stack.push_frame_args(func, args)
     }
 
-    pub fn tail_call(&mut self, _call: Reg) // -> Lresult<Frame>
+    /**
+     * handy accessor function when calling from rust native functions
+     */
+    pub fn get_param(&self, p: i8) -> Lresult<&Val>
     {
+        unsafe { *self.param }.get(p as usize).ok_or_else(|| lfail!(
+            failure::Mode::RuntimeLeemaFailure,
+            "invalid param index",
+            "param": ldisplay!(p),
+        ))
     }
 
-    pub fn get(&self, i: usize) -> Lresult<&Val>
+    /**
+     * handy accessor function when calling from rust native functions
+    pub fn get_params(&self) -> &Struple2Slice<Val>
     {
-        unsafe { Ok((*self.stack).data.get(i).unwrap()) }
+        unsafe { *self.param }
+    }
+     */
+
+    pub fn get_reg(&self, r: Reg) -> Lresult<&Val>
+    {
+        match r {
+            Reg::Params(Ireg::Reg(i)) => {
+                Ok(unsafe { *self.param }.get(i).unwrap())
+            }
+            Reg::Locals(Ireg::Reg(i)) => {
+                Ok(unsafe { *self.local }.get(i).unwrap())
+            }
+            _ => {
+                Err(lfail!(
+                    failure::Mode::RuntimeLeemaFailure,
+                    "cannot get register",
+                    "reg": r,
+                ))
+            }
+        }
     }
 
-    pub fn get_mut(&mut self, i: usize) -> Lresult<&mut Val>
+    pub fn set_reg(&mut self, r: Reg, v: Val) -> Lresult<()>
     {
-        unsafe { Ok((*self.stack).data.get_mut(i).unwrap()) }
-    }
-
-    pub fn set(&mut self, i: usize, d: Val) -> Lresult<()>
-    {
-        unsafe {
-            *(*self.stack).data.get_mut(i).unwrap() = d;
+        match r {
+            Reg::Local(i) => {
+                let primary = i.get_primary() as usize;
+                if primary >= self.locals.len() {
+                    self.locals.resize(primary + 1, Default::default())
+                }
+                self.locals.ireg_set(i, v)
+            }
+            Reg::Stack(i) => {
+                let primary = i.get_primary() as usize;
+                if primary >= self.stack.len() {
+                    self.stack.resize(primary + 1, Default::default())
+                }
+                self.stack.ireg_set(i, v)
+            }
+            Reg::Param(i) => {
+                // debatable whether param should allow writes
+                // might need to reverse this at some point and
+                // copy params before writing
+                // Struple.ireg_set checks bounds
+                self.params.ireg_set(i, v)
+            }
+            _ => {
+                Err(lfail!(
+                    failure::Mode::RuntimeLeemaFailure,
+                    "cannot set register",
+                    "reg": r,
+                ))
+            }
         }
         Ok(())
     }
