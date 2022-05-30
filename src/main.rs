162d01ce4164bc0596a769f35acf93e0bf6d3435
Matthew Graham <mattg@leema.org>
2022-05-30 17:49:19 -0400

    fix the file attributes
    moved the license comment around to avoid cargo fmt complaints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![deny(warnings)]
+#![allow(dead_code)]
+
 /// Copyright 2022 Matthew Graham
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +14,6 @@
 /// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
-#![deny(warnings)]
-#![allow(dead_code)]
 
 #[macro_use]
 mod leema;
